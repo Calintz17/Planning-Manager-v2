82c3e364a983f611fdea82abd1ccbcc6075d4ccc
Calintz17 <roman.attal@gmail.com>
2025-10-23 12:06:18 +0200

    Add example configuration file for Supabase
    This file serves as an example configuration for Supabase.

---
+++ config.example.js
@@ -0,0 +1,4 @@
+// config.example.js
+// Étape 2 : dupliquer ce fichier en "config.js" et remplacer par VRAIES VALEURS
+export const SUPABASE_URL = "https://YOUR-PROJECT.supabase.co";
+export const SUPABASE_ANON_KEY = "YOUR-ANON-KEY";
