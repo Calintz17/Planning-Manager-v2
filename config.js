87c647a6572a1793a1282ebd850a844972363afa
Calintz17 <roman.attal@gmail.com>
2025-10-23 12:27:09 +0200

    Add Supabase configuration to config.js
    Added configuration for Supabase URL and anon key.

---
+++ config.js
@@ -0,0 +1,4 @@
+// config.js
+// ⚠️ Clé "anon" = OK côté navigateur. NE PAS utiliser la service_role ici.
+export const SUPABASE_URL = "https://miawersffeosiovqdokw.supabase.co";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pYXdlcnNmZmVvc2lvdnFkb2t3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyMDY4NTYsImV4cCI6MjA3Njc4Mjg1Nn0.ixCX8Zh8QLxJrnV7ncpNSCv3KtL0ao0PqsFmA6xYYyA";
