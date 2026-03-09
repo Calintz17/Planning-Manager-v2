de29948f50bb89b629239b588b0c892531e1c977
Calintz17 <roman.attal@gmail.com>
2026-03-09 17:18:55 +0100

    Delete config.example.js

--- config.example.js
+++
@@ -1,4 +0,0 @@
-// config.example.js
-// Étape 2 : dupliquer ce fichier en "config.js" et remplacer par VRAIES VALEURS
-export const SUPABASE_URL = "https://YOUR-PROJECT.supabase.co";
-export const SUPABASE_ANON_KEY = "YOUR-ANON-KEY";
